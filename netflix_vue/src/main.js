5d441dd708302cf53597f95a5e5dfa858e67eef1
RillianDiello <rilliansf@gmail.com>
2020-01-06 23:32:57 -0400

    Configurando Vue Router

--- netflix_vue/src/main.js
+++ netflix_vue/src/main.js
@@ -1,9 +1,40 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Home from './Pages/Home.vue'
 import VueResource from 'vue-resource'
-Vue.use(VueResource)
+import VueRouter from 'vue-router'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const Contato = { template: '<p>Paginda de contato em construção.</p>'}
+const Undefine = { template: '<p>Página não encontrada.</p>'}
+
+
+const routes = [
+  {path: '/', component: Home},
+  {path: '/contato', component: Contato},
+]
+
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router  
+}).$mount('#app')
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     current_rote: window.location.pathname
+//   },
+
+//   computed: {
+//     exibeComponent(){
+//       return rotes[this.current_rote] || Undefine
+//     }
+//   },
+
+//   render(h) { return h(this.exibeComponent)}
+// })
